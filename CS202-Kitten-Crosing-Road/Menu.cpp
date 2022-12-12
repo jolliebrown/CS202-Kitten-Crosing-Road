fddcab61228c3b0ffc2ec43fc16058fab911f645
Locopaly <95960924+Locopaly@users.noreply.github.com>
2022-12-12 17:54:56 +0700

    Menu constructor

--- CS202-Kitten-Crosing-Road/Menu.cpp
+++ CS202-Kitten-Crosing-Road/Menu.cpp
@@ -12,7 +12,27 @@ Sprite Button::getSprite()
 
 Menu::Menu(RenderWindow& mWindow) : window(mWindow)
 {
-	
+	onePicLoad(asset, "Media/Menu/Menu.png");
+	onePicLoad(asset, "Media/Menu/CONTINUE.png");
+	onePicLoad(asset, "Media/Menu/PLAY.png");
+	onePicLoad(asset, "Media/Menu/SETTINGS.png");
+	onePicLoad(asset, "Media/Menu/INSTRUCTION.png");
+	onePicLoad(asset, "Media/Menu/QUIT.png");
+	onePicLoad(asset, "Media/Menu/CONTINUE_here.png");
+	onePicLoad(asset, "Media/Menu/PLAY_here.png");
+	onePicLoad(asset, "Media/Menu/SETTINGS_here.png");
+	onePicLoad(asset, "Media/Menu/INSTRUCTION_here.png");
+	onePicLoad(asset, "Media/Menu/QUIT_here.png");
+
+	Sprite sprite(asset[0]);
+	sprite.setPosition(Vector2f(BaseUnit * 31, BaseUnit * 19));
+	background.push_back(sprite);
+	for (int i = 1; i < 6; ++i)
+	{
+		Button button(window, asset[i], BaseUnit * 32, BaseUnit * (19.5 + i));
+		buttons.push_back(button);
+	}
+	loadCursor();
 }
 
 void Menu::handleEvent(Event& event, vector<Menu*>& mMenu)
@@ -27,7 +47,16 @@ void Menu::draw()
 
 void Menu::loadCursor()
 {
-	
+	Image image;
+	image.loadFromFile("Media/Menu/normalMouse.png");
+	Cursor* cursor;
+	cursor = new Cursor;
+	cursor->loadFromPixels(image.getPixelsPtr(), Vector2u(16, 16), Vector2u(0, 0));
+	cursorTexture.push_back(cursor);
+	cursor = new Cursor;
+	image.loadFromFile("Media/Menu/pointingMouse.png");
+	cursor->loadFromPixels(image.getPixelsPtr(), Vector2u(16, 16), Vector2u(0, 0));
+	cursorTexture.push_back(cursor);
 }
 
 void Menu::loadAsset()
